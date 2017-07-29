958134d34d6e11ba4c6cae1de951b274edbf093b
Derek Miranda <derek.miranda.1992@gmail.com>
2017-07-29 10:57:26 -0700

    App rendering tests

--- client/components/__tests__/App.spec.js
+++ client/components/__tests__/App.spec.js
@@ -4,7 +4,16 @@ import { shallow } from 'enzyme';
 import App from '../App';
 import MovieTable from '../MovieTable';
 
-test('It should render', t => {
-  const wrapper = shallow(<App />);
+test.beforeEach(t => {
+  t.context.wrapper = shallow(<App />);
+})
+
+test('App renders properly', t => {
+  const { wrapper } = t.context;
   t.is(wrapper.length, 1);
+})
+
+test('App renders MovieTable', t => {
+  const { wrapper } = t.context;
+  t.is(wrapper.find(MovieTable).length, 1);
 })
