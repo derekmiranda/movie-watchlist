af321ab72eab670f8876d93f03e7b751236950e6
Derek Miranda <derek.miranda.1992@gmail.com>
2017-07-29 13:14:33 -0700

    MovieTable header tests

--- client/components/MovieTable.js
+++ client/components/MovieTable.js
@@ -3,7 +3,13 @@ import React from 'react';
 const MovieTable = () => {
   return (
     <table>
-    
+      <tr className='header'>
+        <th>Title</th>
+        <th>Director</th>
+        <th>Actors</th>
+        <th>Tags</th>
+        <th>Notes</th>
+      </tr>
     </table>
   )
 }
--- client/components/__tests__/MovieTable.spec.js
+++ client/components/__tests__/MovieTable.spec.js
@@ -16,3 +16,21 @@ test('MovieTable renders a table', t => {
   const { wrapper } = t.context;
   t.is(wrapper.find('table').length, 1);
 })
+
+test('Table has a header with correct fields', t => {
+  const { wrapper } = t.context;
+  const tableRows = wrapper.find('tr');
+  const numRows = tableRows.length;
+  t.truthy(numRows >= 1, 'No table header');
+
+  const tableHead = tableRows.first();
+  const expectedFields = ['Title', 'Director', 'Actors', 'Tags', 'Notes'];
+  const headerFields = tableHead.children();
+
+  t.is(expectedFields.length, headerFields.length, 'Not enough header fields');
+  headerFields.forEach((field, i) => {
+    const fieldText = field.text();
+    const expectedField = expectedFields[i];
+    t.truthy(fieldText.includes(expectedField), `Expected ${field} in header`);
+  })
+})
