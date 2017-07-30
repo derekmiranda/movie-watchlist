b00898947f5d4023000632ffade3fa2e57f4abe0
Derek Miranda <derek.miranda.1992@gmail.com>
2017-07-29 18:43:35 -0700

    Added top-level imports for App

--- client/index.js
+++ client/index.js
@@ -1,6 +1,20 @@
 import React, { Component } from 'react';
 import ReactDom from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
 import App from './components/App';
+
+const middleware = [];
+
+if (process.env.NODE_ENV === 'dev') {
+  const { logger } = require('redux-logger');
+  middleware.push(logger);
+}
+
+// const store = createStore(
+//   rootReducer,
+//   applyMiddleware(...middleware)
+// )
 
 ReactDom.render(
   <App />,
