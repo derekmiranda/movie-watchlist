1091ffd4b09fc14b8af3f08682a845c9a5834207
Derek Miranda <derek.miranda.1992@gmail.com>
2017-07-30 20:45:52 -0700

    Using movie reducer to process sample movie json

--- client/reducers/movies/__tests__/movies.spec.js
+++ client/reducers/movies/__tests__/movies.spec.js
@@ -1,5 +1,6 @@
 import test from 'ava';
 import movies from '..';
+import movie from '../movie';
 import Immutable from 'seamless-immutable';
 import {
   addMovie, removeMovie, editSingleValue
@@ -17,7 +18,7 @@ test('Can add new movie to empty list', t => {
   const newMovie = t.context.sampleMovies[0];
 
   const actualList = movies(undefined, addMovie(newMovie));
-  const expectedList = [newMovie];
+  const expectedList = [movie(newMovie)];
 
   t.deepEqual(actualList, expectedList, 'Movie not added correctly');
 })
@@ -28,14 +29,17 @@ test(`Adding new movie doesn't remove previous ones`, t => {
   const newMovie = sampleMovies[sampleMovies.length - 1];
 
   const actualList = movies(initMovies, addMovie(newMovie));
+  const expectedList = sampleMovies.map(rawMovie => movie(rawMovie));
 
-  t.deepEqual(actualList, sampleMovies, 'Movie not added correctly');
+  t.deepEqual(actualList, expectedList, 'Movie not added correctly');
 })
 
 test('Can remove movie from list', t => {
   const { sampleMovies } = t.context;
   const targetIdx = 1;
-  const expectedList = sampleMovies.filter((_, idx) => idx !== targetIdx);
+  const expectedList = sampleMovies
+    .filter((_, idx) => idx !== targetIdx)
+    .map(rawMovie => movie(rawMovie))
   const actualList = movies(sampleMovies, removeMovie(targetIdx));
 
   t.deepEqual(actualList, expectedList, 'Movie not removed correctly');
