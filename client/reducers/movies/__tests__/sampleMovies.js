71feef691320a6912f9de6cbf88d2dc957987aed
Derek Miranda <derek.miranda.1992@gmail.com>
2017-07-30 18:15:12 -0700

    Added sample movie array

---
+++ client/reducers/movies/__tests__/sampleMovies.js
@@ -0,0 +1,23 @@
+module.exports = [
+  {
+    title: 'Toy Story',
+    director: 'John Lasseter',
+    actors: ['Tom Hanks', 'Tim Allen'],
+    tags: ['Animated', '3D', 'Comedy'],
+    notes: 'G.O.A.T.',
+  },
+  {
+    title: 'Toy Story 2',
+    director: 'John Lasseter, Lee Unkrich, Ash Brannon',
+    actors: ['Still Tom Hanks', 'and Tim Allen'],
+    tags: ['Animated', '3D', 'Comedy'],
+    notes: 'Also G.O.A.T.',
+  },
+  {
+    title: 'Toy Story 3',
+    director: 'Lee Unkrich',
+    actors: ['Still Tom Hanks', 'and Tim Allen'],
+    tags: ['Animated', '3D', 'Comedy'],
+    notes: 'Not bad',
+  },
+]
