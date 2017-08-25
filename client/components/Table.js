2fe588b406629547ac745db1c3b76ae0a7f6fbeb
Derek Miranda <derek.miranda.1992@gmail.com>
2017-08-25 11:29:04 -0700

    Clarified fields to orderedFields prop for determining order to display values

--- client/components/Table.js
+++ client/components/Table.js
@@ -1,18 +1,25 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import TableHeader from './TableHeader';
 import TableBody from './TableBody';
 
-const Table = ({ fields, contacts }) => {
-  const rows = contacts && contacts.map((contact) => {
-    return fields.map(field => contact[field]);
+const Table = ({ labels, orderedFields, objs }) => {
+  const rows = objs && objs.map((obj) => {
+    return orderedFields.map(field => obj[field]);
   })
 
   return (
     <table>
-      <TableHeader fields={fields} />
-      <TableBody fields={fields} rows={rows} />
+      <TableHeader labels={labels} />
+      <TableBody fields={orderedFields} rows={rows} />
     </table>
   )
+}
+
+Table.propTypes = {
+  labels: PropTypes.arrayOf(PropTypes.string),
+  fields: PropTypes.arrayOf(PropTypes.string),
+  objs: PropTypes.arrayOf(PropTypes.object),
 }
 
 export default Table;
