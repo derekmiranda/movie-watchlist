ca6e1f762381c248879d78c6c4dbe247d0f98f7f
Derek Miranda <derek.miranda.1992@gmail.com>
2017-08-24 20:41:40 -0700

    Fixed Row specs for AVA

---
+++ client/components/__tests__/Row.spec.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import test from 'ava';
+import { shallow } from 'enzyme';
+import Row from '../Row';
+
+test('should render a tr', t => {
+  const emptyHeaderWrap = shallow(<Row />);
+  const trWrap = emptyHeaderWrap.find('tr');
+  t.is(trWrap.length, 1);
+})
+
+test('should render a td for each passed in item', t => {
+  const values = ['something', 'another', 'thing'];
+  const filledHeaderWrap = shallow(<Row values={values}/>);
+  const tdWrap = filledHeaderWrap.find('td');
+  t.is(tdWrap.length, values.length);
+})
