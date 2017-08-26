6f2904cc71d73798beec896186fcbf2031c64803
Derek Miranda <derek.miranda.1992@gmail.com>
2017-08-25 18:54:24 -0700

    Created MoviesTable container to pass movies data to table

---
+++ client/containers/MoviesTable.js
@@ -0,0 +1,23 @@
+import { connect } from 'react-redux';
+import Table from '../components/Table';
+
+const orderedFields = [
+  'title',
+  'director',
+  'genres',
+  'actors',
+  'notes',
+]
+
+const capitalize = str => str[0].toUpperCase() + str.slice(1);
+const labels = orderedFields.map(capitalize);
+
+const mapStateToProps = (state) => ({
+  orderedFields,
+  labels,
+  objs: state.movies,
+})
+
+const MoviesTable = connect(mapStateToProps)(Table);
+
+export default MoviesTable;
