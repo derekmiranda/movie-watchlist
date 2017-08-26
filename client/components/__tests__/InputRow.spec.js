4bed5c464309600d70ea191a786b603698456a7f
Derek Miranda <derek.miranda.1992@gmail.com>
2017-08-25 20:33:05 -0700

    Removed req props testing

--- client/components/__tests__/InputRow.spec.js
+++ client/components/__tests__/InputRow.spec.js
@@ -7,23 +7,10 @@ import Row from '../Row';
 
 const fields = ['title', 'director', 'genres'];
 
-test.before(t => {
-  sinon.stub(console, 'error').throws(function() { return new Error() });
-})
-
 test('should have a Row', t => {
   const wrapper = shallow(<InputRow fields={fields} />);
   const rowWrap = wrapper.find(Row);
   t.is(rowWrap.length, 1);
-})
-
-test('requires fields array', t => {
-  const fn = () => shallow(<InputRow />);
-  t.throws(fn, 'Should warn about absence of fields');
-})
-
-test.after(t => {
-  sinon.stub(console, 'error').restore();
 })
 
 test.todo('should have an input for every field');
