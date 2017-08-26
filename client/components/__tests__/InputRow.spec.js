cd8a12262df64780c71a4a0741274659739d65ce
Derek Miranda <derek.miranda.1992@gmail.com>
2017-08-26 00:08:45 -0700

    Fixed InputRow instance

--- client/components/__tests__/InputRow.spec.js
+++ client/components/__tests__/InputRow.spec.js
@@ -5,9 +5,10 @@ import InputRow from '../InputRow';
 import Row from '../Row';
 
 const fields = ['title', 'director', 'genres'];
+const values = ['Taxi Driver', 'Martin Scorsese', 'Comedy'];
 
 test('should have a Row', t => {
-  const wrapper = shallow(<InputRow fields={fields} />);
+  const wrapper = shallow(<InputRow fields={fields} values={values} />);
   const rowWrap = wrapper.find(Row);
   t.is(rowWrap.length, 1);
 })
