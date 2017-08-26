4395773971fb424ce8eb7485cac80d9d0f1b9701
Derek Miranda <derek.miranda.1992@gmail.com>
2017-08-25 19:19:51 -0700

    Body properly defaults to empty row

--- client/components/TableBody.js
+++ client/components/TableBody.js
@@ -3,17 +3,21 @@ import PropTypes from 'prop-types';
 import Row from './Row';
 
 const TableBody = ({ fields, rows }) => {
-  const rowElems = rows && rows.map((row, i) => <Row key={i} items={row} />)
-  
+
   function createDefaultRow() {
     const emptyRowValues = new Array(fields.length).fill(null);
     const defaultRow = <Row items={emptyRowValues} />;
     return defaultRow;
   }
+
+  const rowElems = rows.length ?
+    rows.map((row, i) => <Row key={i} items={row} />)
+    :
+    createDefaultRow();
   
   return (
     <tbody>
-      { rowElems || createDefaultRow() }
+      { rowElems }
     </tbody>
   )
 }
