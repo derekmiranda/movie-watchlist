c7253e729650cdf839138168215e5d84bf186211
Derek Miranda <derek.miranda.1992@gmail.com>
2017-08-27 13:06:26 -0700

    Display fields in order in header

--- client/components/TableHeader.js
+++ client/components/TableHeader.js
@@ -2,8 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const TableHeader = ({ labels }) => {
+  const headerCells = labels.map((label, i) => (
+    <td key={i}>
+      <h2>{label}</h2>
+    </td>
+  ))
+
   return (
     <thead>
+      <tr>
+        { headerCells }
+      </tr>
     </thead>
   )
 }
