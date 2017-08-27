9f5937373d1bc4196f8bfec7a5614383b58ed97f
Derek Miranda <derek.miranda.1992@gmail.com>
2017-08-27 13:02:49 -0700

    Generic InputRow class

--- client/components/InputRow.js
+++ client/components/InputRow.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const InputRow = () => {
-  return <div />;
+const InputRow = ({ orderedFields, obj }) => {
+  const cells = orderedFields.map((field, i) => (
+    <td key={i}>
+      <input name={field} value={obj[field]} />
+    </td>
+  ))
+
+  return (
+    <tr>
+      { cells }
+    </tr>
+  )
 }
 
 InputRow.propTypes = {
-
+  orderedFields: PropTypes.arrayOf(PropTypes.string).isRequired,
+  obj: PropTypes.object.isRequired,
+  idx: PropTypes.number,
 }
 
 export default InputRow;
