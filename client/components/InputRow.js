9b3ffc8dc0fa166804ca80a132c07877b5cb8a3e
Derek Miranda <derek.miranda.1992@gmail.com>
2017-08-27 13:42:46 -0700

    Can inject extra elems into row

--- client/components/InputRow.js
+++ client/components/InputRow.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const InputRow = ({ orderedFields, obj }) => {
-  const cells = orderedFields.map((field, i) => (
+const InputRow = ({ orderedFields, obj, extraElems }) => {
+  const inputs = orderedFields.map((field) => (
+    <input name={field} value={obj[field]} />
+  ))
+
+  const items = inputs.concat(extraElems || []);
+  const cells = items.map((item, i) => (
     <td key={i}>
-      <input name={field} value={obj[field]} />
+      {item}
     </td>
   ))
 
@@ -19,6 +24,7 @@ InputRow.propTypes = {
   orderedFields: PropTypes.arrayOf(PropTypes.string).isRequired,
   obj: PropTypes.object.isRequired,
   idx: PropTypes.number,
+  extraElems: PropTypes.arrayOf(PropTypes.element),
 }
 
 export default InputRow;
