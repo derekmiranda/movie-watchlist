9a00257bb0292c2b60e1bf638e27dc1141ce557a
Derek Miranda <derek.miranda.1992@gmail.com>
2017-08-28 16:10:43 -0700

    Added redux multi and logger middlewares

--- client/index.js
+++ client/index.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
 import ReactDom from 'react-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
+import reduxMulti from 'redux-multi';
+import { batchedSubscribe } from 'redux-batched-subscribe';
 import App from './components/App';
 import rootReducer from './reducers';
 
-const middleware = [];
+const middleware = [
+  reduxMulti,  
+];
 
 if (process.env.NODE_ENV === 'development') {
   const { logger } = require('redux-logger');
   middleware.push(logger);
 }
 
-const store = createStore(rootReducer);
+const createStoreWithMiddleware = applyMiddleware(...middleware)(createStore);
+const finalCreateStore = batchedSubscribe(fn => fn())(createStoreWithMiddleware);
+const store = finalCreateStore(rootReducer);
 
 ReactDom.render(
   <Provider store={store}>
