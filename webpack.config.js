dd401480c95fe613d501602fa0c2942400cbc91f
Derek Miranda <derek.miranda.1992@gmail.com>
2017-08-28 16:10:53 -0700

    Proper access to env vars

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,12 @@ module.exports = {
     publicPath: 'dist',
   },
   plugins: [
-    new webpack.HotModuleReplacementPlugin()
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+      },
+    }),
   ],
   module: {
     loaders: [
