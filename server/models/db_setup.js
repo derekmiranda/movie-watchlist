63b07a37cc960d96482c7686c02582ea022f8fc4
Derek Miranda <derek.miranda.1992@gmail.com>
2017-08-29 16:52:19 -0700

    Tests db connection

--- server/models/db_setup.js
+++ server/models/db_setup.js
@@ -1,19 +1,10 @@
 const Sequelize = require('sequelize');
 const dbConfigMap = require('../../config/db.json');
+
 const dbConfig = dbConfigMap[process.env.NODE_ENV] || dbConfigMap.development;
+const sequelize = new Sequelize(dbConfig);
 
-const {
-  host,
-  dialect,
-  username,
-  password,
-  database,
-} = dbConfig;
-
-const sequelize = new Sequelize({
-  host,
-  dialect,
-  username,
-  password,
-  database,
-})
+sequelize
+  .authenticate()
+  .then(() => console.log(`Connected to ${dialect} database`))
+  .catch(err => { throw err; });
