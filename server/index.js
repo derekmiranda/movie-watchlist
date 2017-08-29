3296a5d2bcc42eef38c8171dd63d4dd5d4e81a1b
Derek Miranda <derek.miranda.1992@gmail.com>
2017-08-29 00:43:24 -0700

    Added moviesRouter

--- server/index.js
+++ server/index.js
@@ -1,9 +1,12 @@
 const path = require('path');
 const express = require('express');
+const moviesRouter = require('./routers/moviesRouter');
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+
+app.use('/movies', moviesRouter);
 
 app.use(express.static('public'));
 
