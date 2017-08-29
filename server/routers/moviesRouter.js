efdd7d0663f6b0bf400f60f9dbe394b554fb9935
Derek Miranda <derek.miranda.1992@gmail.com>
2017-08-29 00:43:18 -0700

    Put routers and controllers into own folders

---
+++ server/routers/moviesRouter.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const moviesController = require('../controllers/moviesController');
+
+const moviesRouter = express.Router();
+
+// moviesRouter.get('/')
+
+module.exports = moviesRouter;
