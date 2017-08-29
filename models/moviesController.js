2c656928a9bc13c6558ab3916b92e0c12c0450ae
Derek Miranda <derek.miranda.1992@gmail.com>
2017-08-29 00:23:29 -0700

    MoviesController skeleton

---
+++ models/moviesController.js
@@ -0,0 +1,8 @@
+const moviesController = {};
+
+moviesController.getMovies = () => {}
+moviesController.postMovie = () => {}
+moviesController.updateMovies = () => {}
+moviesController.removeMovie = () => {}
+
+module.exports = moviesController;
