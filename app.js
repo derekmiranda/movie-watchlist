d803275dfc8b2ba368c84951f0f068b9815799a8
Derek Miranda <derek.miranda.1992@gmail.com>
2017-08-30 13:58:24 -0700

    Added req body parsers

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const express = require('express');
+const bodyParser = require('body-parser');
 const moviesRouter = require('./routers/moviesRouter');
 const db = require('./models');
 
@@ -8,6 +9,8 @@ async function main() {
   const PORT = process.env.PORT || 3000;
   const app = express();
   
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
   app.use('/movies', moviesRouter);
   
   app.use(express.static('public'));
