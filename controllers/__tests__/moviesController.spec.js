ad95b5cf3b5cf9fab939151761200774de584d2d
Derek Miranda <derek.miranda.1992@gmail.com>
2017-08-30 10:19:11 -0700

    Checks for default vals

--- controllers/__tests__/moviesController.spec.js
+++ controllers/__tests__/moviesController.spec.js
@@ -1,4 +1,5 @@
 import test from 'ava';
+import _ from 'lodash';
 import moviesController from '../moviesController';
 
 test.beforeEach('Initialize db', async t => {
@@ -7,15 +8,14 @@ test.beforeEach('Initialize db', async t => {
   t.context.db = db;
 })
 
-test('db should authenticate', t => {
+test.serial('db should authenticate', t => {
   const { sequelize } = t.context.db;
-  sequelize
-    .authenticate()
+  sequelize.authenticate()
     .then(t.pass())
     .catch(err => { throw err; })
 })
 
-test('should contain given methods', t => {
+test.serial('should contain given methods', t => {
   const methods = [
     'getMovies',
     'postMovie',
@@ -29,12 +29,55 @@ test('should contain given methods', t => {
   })
 })
 
-test.only('can get all movies', async t => {
+test.serial('can get all movies', async t => {
+  const movies = [
+    { title: 'Toy Story' },
+    { title: 'Toy Story 2' },
+  ];
   const Movie = t.context.db.movie;
   try {
-    const movies = await Movie.findAll();
-    t.pass();
+    const res = await Movie.bulkCreate(movies);
+    const foundMovies = await Movie.findAll();
+
+    const byId = (a, b) => a.id > b.id;
+    foundMovies
+      .sort(byId)
+      .forEach((foundMovie, i) => {
+        const movie = movies[i];
+        Object.keys(movie).forEach((key) => {
+          t.is(movie[key], foundMovie[key]);
+        })
+      })
   } catch (err) {
     throw err;
   }
+})
+
+test.serial('should default non-title values to empty string', async t => {
+  const movie = {
+    title: 'Toy Story'
+  };
+  const Movie = t.context.db.movie;
+  try {
+    const res = await Movie.create(movie);
+    const firstMovie = await Movie.find();
+    const firstMovieData = _.pick(firstMovie, [
+      'id', 'title', 'director', 'genres', 'actors', 'notes'
+    ])
+    t.deepEqual(firstMovieData, {
+      id: 1,
+      title: 'Toy Story',
+      director: '',
+      genres: '',
+      actors: '',
+      notes: '',
+    });
+  } catch (err) {
+    throw err;
+  }
+})
+
+test.afterEach(async t => {
+  // clear database for next test
+  await t.context.db.sequelize.sync({ force: true });
 })
