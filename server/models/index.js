caeebfaee0eeb7ed9e02ee38e48aa38839dbef11
Derek Miranda <derek.miranda.1992@gmail.com>
2017-08-29 18:10:57 -0700

    Imports neighboring models

--- server/models/index.js
+++ server/models/index.js
@@ -6,3 +6,16 @@ const dbConfigMap = require(dbConfigPath);
 
 const dbConfig = dbConfigMap[process.env.NODE_ENV] || dbConfigMap.development;
 const sequelize = new Sequelize(dbConfig);
+const db = {};
+
+fs.readdirSync(__dirname)
+  .filter(file => file.startsWith('.') && file !== 'index.js')
+  .forEach((file) => {
+    const model = sequelize.import(path.join(__dirname, file));
+    db[model.name] = model;
+  });
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
