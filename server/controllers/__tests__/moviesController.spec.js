54e435bca8d1afb0fcba0d725df9ac9fc2a7b64e
Derek Miranda <derek.miranda.1992@gmail.com>
2017-08-29 21:07:49 -0700

    Controller uses in-memory sqlite db

--- server/controllers/__tests__/moviesController.spec.js
+++ server/controllers/__tests__/moviesController.spec.js
@@ -2,7 +2,6 @@ import test from 'ava';
 import db from '../../models';
 
 test('Test if db works', t => {
-  console.log(process.env.NODE_ENV)
   const { sequelize } = db;
   sequelize
     .authenticate()
