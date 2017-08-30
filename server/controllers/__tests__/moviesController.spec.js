9585f18e4c8ef9c3a6a1cdf42d5664f506558da5
Derek Miranda <derek.miranda.1992@gmail.com>
2017-08-29 21:42:15 -0700

    Asserts controllers necessary methods; initial test to check for getting movies

--- server/controllers/__tests__/moviesController.spec.js
+++ server/controllers/__tests__/moviesController.spec.js
@@ -1,10 +1,36 @@
 import test from 'ava';
 import db from '../../models';
+import moviesController from '../moviesController';
 
-test('Test if db works', t => {
+test('db should authenticate', t => {
   const { sequelize } = db;
   sequelize
     .authenticate()
     .then(t.pass())
     .catch(err => { throw err; })
+})
+
+test('should contain given methods', t => {
+  const methods = [
+    'getMovies',
+    'postMovie',
+    'updateMovies',
+    'removeMovie',
+  ]
+
+  methods.forEach((method) => {
+    t.true(method in moviesController);
+    t.is(typeof moviesController[method], 'function');
+  })
+})
+
+test.only('can get all movies', async function(t) {
+  const Movie = db.movie;
+  try {
+    const movies = await Movie.findAll();
+    console.log('movies:', movies);
+    t.pass();
+  } catch (err) {
+    throw err;
+  }
 })
