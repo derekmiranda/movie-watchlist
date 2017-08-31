d7ce93953f08cd3d3c60060314fd89687fe35047
Derek Miranda <derek.miranda.1992@gmail.com>
2017-08-31 08:57:46 -0700

    Light wrapper around Table for fetching movies

--- client/containers/MoviesTable.js
+++ client/containers/MoviesTable.js
@@ -1,3 +1,4 @@
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Table from '../components/Table';
 
@@ -6,6 +7,25 @@ const mapStateToProps = (state) => ({
   newMovie: state.newMovie,
 })
 
-const MoviesTable = connect(mapStateToProps)(Table);
+class FetchTable extends Component {
+  
+  componentDidMount() {
+    async function fetchMovies() {
+      const res = await fetch(process.env.MOVIES_API_URL);
+      console.log(res);
+      const movies = await res.json();
+      console.log(movies);
+    }
+    fetchMovies();
+  }
+
+  render() {
+    return (
+      <Table {...this.props} />
+    )
+  }
+}
+
+const MoviesTable = connect(mapStateToProps)(FetchTable);
 
 export default MoviesTable;
