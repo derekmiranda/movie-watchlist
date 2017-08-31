7344bfb9b2591222ba275f993c940c354884e102
Derek Miranda <derek.miranda.1992@gmail.com>
2017-08-31 08:21:27 -0700

    Made separate module for development webpack settings

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,7 @@
 const path = require('path');
 const webpack = require('webpack');
+const webpackDev = require('./webpack.development');
 
 const WDS_PORT = 8000;
 
-module.exports = {
-  entry: [
-    './client',
-  ],
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'public/dist'),
-    publicPath: 'dist',
-  },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify(process.env.NODE_ENV),
-      },
-    }),
-  ],
-  module: {
-    loaders: [
-      {
-        test: /\.(jsx?)$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/,
-        include: [ path.resolve(__dirname, './client') ]
-      },
-      {
-        test: /(\.css|\.scss|\.sass)$/,
-        include: [ path.resolve(__dirname, './client/css') ],
-        loaders: ['style-loader', 'css-loader', 'sass-loader'],
-      },
-    ]
-  },
-  devServer: {
-    contentBase: './public',
-    inline: true,
-    hot: true,
-  },
-}
+module.exports = webpackDev;
---
+++ webpack.development.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const webpack = require('webpack');
+
+const WDS_PORT = 8000;
+
+module.exports = {
+  entry: [
+    './client',
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public/dist'),
+    publicPath: 'dist',
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+        API_URL: 'localhost:3000/movies',
+      },
+    }),
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.(jsx?)$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        include: [ path.resolve(__dirname, './client') ]
+      },
+      {
+        test: /(\.css|\.scss|\.sass)$/,
+        include: [ path.resolve(__dirname, './client/css') ],
+        loaders: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ]
+  },
+  devServer: {
+    contentBase: './public',
+    inline: true,
+    hot: true,
+  },
+}
