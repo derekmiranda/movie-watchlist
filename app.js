b60c05e711fc8ff05dbea0c7f4edfaabb7bee0c7
Derek Miranda <derek.miranda.1992@gmail.com>
2017-08-31 08:57:07 -0700

    Removed logic from async function

--- app.js
+++ app.js
@@ -4,18 +4,17 @@ const bodyParser = require('body-parser');
 const moviesRouter = require('./routers/moviesRouter');
 const db = require('./models');
 
-async function main() {
-  await db.syncPromise();
-  const PORT = process.env.PORT || 3000;
-  const app = express();
-  
-  app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded({ extended: true }));
-  app.use('/movies', moviesRouter);
-  
-  app.use(express.static('public'));
-  
-  app.listen(PORT, () => console.log(`Listening on ${PORT}`));
-}
+db.syncPromise()
+  .then(() => console.log('DB synced'))
+  .catch((err) => { throw err; });
 
-main();
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/movies', moviesRouter);
+
+app.use(express.static('public'));
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
