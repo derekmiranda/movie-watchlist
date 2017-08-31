eacf9651e03cf682fc5e1161c4dda818dbe9ea69
Derek Miranda <derek.miranda.1992@gmail.com>
2017-08-31 00:14:38 -0700

    Added patch route to update movies

--- routers/moviesRouter.js
+++ routers/moviesRouter.js
@@ -3,17 +3,31 @@ const moviesController = require('../controllers/moviesController');
 
 const moviesRouter = express.Router();
 
-const errorResponse = ({ res, err, statusCode }) => res.status(statusCode).end(err);
+const errorResponse = ({ res, err, statusCode }) => res.status(statusCode).json(err);
+const JSONFormat = data => JSON.stringify(data, null, 4);
+const formattedJSONResponse = (res, data) => (
+  res
+    .header('Content-Type', 'application/json')
+    .end(JSONFormat(data))
+);
 
 moviesRouter.get('/', (req, res) => moviesController.getMovies()
-  .then(movies => res.json(movies))
+  .then(movies => formattedJSONResponse(res, movies))
   .catch(err => errorResponse({ res, err, statusCode: 500 }))
 );
 
 moviesRouter.post('/', (req, res) => {
   const { movie } = req.body;
   return moviesController.addMovie(movie)
-    .then(result => res.json(result))
+    .then(created => formattedJSONResponse(res, created))
+    .catch(err => errorResponse({ res, err, statusCode: 500 }))
+})
+
+moviesRouter.patch('/:id', (req, res) => {
+  const { id } = req.params;
+  const { movie } = req.body;
+  return moviesController.updateMovie(id, movie)
+    .then(result => formattedJSONResponse(result))
     .catch(err => errorResponse({ res, err, statusCode: 500 }))
 })
 
