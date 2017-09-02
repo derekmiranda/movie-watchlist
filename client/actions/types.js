0e8294066c4b0a999e45eb8efd2c7cd6b23dd28b
Derek Miranda <derek.miranda.1992@gmail.com>
2017-09-01 20:38:00 -0700

    Added fetching types

--- client/actions/types.js
+++ client/actions/types.js
@@ -1,6 +1,12 @@
+// movies
 export const ADD_MOVIE = 'ADD_MOVIE';
 export const EDIT_NEW_MOVIE_VALUE = 'EDIT_NEW_MOVIE_VALUE';
 export const UPDATE_VALUE = 'UPDATE_VALUE';
 export const DELETE_MOVIE = 'DELETE_MOVIE';
 export const CLEAR_NEW_MOVIE = 'CLEAR_NEW_MOVIE';
 export const SAVE_MOVIES = 'SAVE_MOVIES';
+
+// fetching
+export const FETCH_STARTED = 'FETCH_STARTED';
+export const FETCH_SUCCEEDED = 'FETCH_SUCCEEDED';
+export const FETCH_FAILED = 'FETCH_FAILED';
