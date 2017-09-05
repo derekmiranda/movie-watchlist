4b93b98987f070646ca344e32d87206172d64fcd
Derek Miranda <derek.miranda.1992@gmail.com>
2017-09-04 18:12:38 -0700

    Basic fetch saga

--- client/sagas/index.js
+++ client/sagas/index.js
@@ -1,8 +1,22 @@
-import { call, put } from 'redux-saga/effects';
+import { call, put, takeLatest } from 'redux-saga/effects';
+
 import { fetchMovies } from '../services/moviesService';
+import { FETCH_STARTED } from '../actions/types';
 import { fetchSucceeded } from '../actions/creators';
 
 export function* fetchData(action) {
-  const movies = yield call(fetchMovies);
-  yield put(fetchSucceeded(movies));
+  try {
+    const movies = yield call(fetchMovies);
+    yield put(fetchSucceeded(movies));
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+function* watchFetchData() {
+  yield takeLatest(FETCH_STARTED, fetchData);
+}
+
+export default function* rootSaga() {
+  yield watchFetchData();
 }
