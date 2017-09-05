3f6c90457ad24141b91596f67c4b86574e89fa20
Derek Miranda <derek.miranda.1992@gmail.com>
2017-09-05 09:13:58 -0700

    Added update movies method to api

--- client/services/moviesApi.js
+++ client/services/moviesApi.js
@@ -2,3 +2,15 @@ export const fetchMovies = () => {
   return fetch(process.env.MOVIES_API_URL)
     .then(res => res.json())
 }
+
+export const updateMovies = (movies) => {
+  const patchPromises = movies.map((movie) => {
+    return fetch(process.env.MOVIES_API_URL, {
+      method: 'PATCH',
+      body: movie,
+    })
+      .then(res => res.json())
+  })
+
+  return Promise.all(patchPromises);
+}
