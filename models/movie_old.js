f0fb9aae4b3ea5f76f2c9a4effff0a596a415f48
Derek Miranda <derek.miranda.1992@gmail.com>
2017-09-08 08:53:11 -0700

    Keeping this to transfer over to new Movie model

---
+++ models/movie_old.js
@@ -0,0 +1,28 @@
+module.exports = function(sequelize, DataTypes) {
+  const makeStringDefn = () => ({
+    type: DataTypes.STRING,
+    defaultValue: ''
+  })
+
+  const Movie = sequelize.define('movie', {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        notEmpty: true,
+      },
+    },
+    director: makeStringDefn(),
+    genres: makeStringDefn(),
+    actors: makeStringDefn(),
+    notes: makeStringDefn(),
+  })
+
+  return Movie;
+}
