6010d2e425d24d6f49fb705a626cee722d9cf6f5
Derek Miranda <derek.miranda.1992@gmail.com>
2017-09-14 08:36:40 -0700

    App runs once db synced

--- app.js
+++ app.js
@@ -7,24 +7,27 @@ const db = require('./models');
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-// CORS
-app.use((req, res, next) => {
-  res.header({
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Methods': 'GET, POST, PATCH, DELETE',
-  });
-  next();
-})
-
-app.use('/movies', moviesRouter);
-
-app.use(express.static('public'));
-
-app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+function main() {
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+  
+  // CORS
+  app.use((req, res, next) => {
+    res.header({
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PATCH, DELETE',
+    });
+    next();
+  })
+  
+  app.use('/movies', moviesRouter);
+  
+  app.use(express.static('public'));
+  
+  app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+}
 
 db.syncPromise()
   .then(() => console.log('DB setup'))
+  .then(main)
   .catch(err => { throw err });
