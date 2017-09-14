15a364688a45ae5ba279cf09aebaea8f3c6359e3
Derek Miranda <derek.miranda.1992@gmail.com>
2017-09-14 07:41:54 -0700

    Validate email

--- models/user.js
+++ models/user.js
@@ -1,6 +1,11 @@
 'use strict';
 module.exports = function(sequelize, DataTypes) {
   var User = sequelize.define('User', {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
     username: {
       type: DataTypes.STRING,
       unique: true,
@@ -12,18 +17,19 @@ module.exports = function(sequelize, DataTypes) {
     passHash: DataTypes.STRING,
     firstName: DataTypes.STRING,
     lastName: DataTypes.STRING,
-    id: {
-      type: DataTypes.INTEGER,
-      autoIncrement: true,
-      primaryKey: true,
+    email: {
+      type: DataTypes.STRING,
+      validate: {
+        isEmail: true,
+      }
     },
-    email: DataTypes.STRING
   }, {
     classMethods: {
       associate: function(models) {
         User.hasMany(models.Movie);
       }
-    }
+    },
+    // validation instance method(s)
   });
   return User;
 };
