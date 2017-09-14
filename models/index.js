1355077a18ef902aa443919353c27235521687a4
Derek Miranda <derek.miranda.1992@gmail.com>
2017-09-14 06:17:00 -0700

    Makes associations

--- models/index.js
+++ models/index.js
@@ -10,12 +10,18 @@ const sequelize = new Sequelize(dbConfig);
 const db = {};
 
 fs.readdirSync(__dirname)
-  .filter(file => !file.startsWith('.') && !file.startsWith('_') && file !== 'index.js' )
+  .filter(file => !file.startsWith('.') && !file.startsWith('_') && file !== 'index.js')
   .forEach((file) => {
     const model = sequelize.import(path.join(__dirname, file));
     db[model.name] = model;
   });
-  
+
+Object.keys(db).forEach((modelName) => {
+  if ("associate" in db[modelName]) {
+    db[modelName].associate(db);
+  }
+});
+
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 db.syncPromise = () => sequelize.sync();
