c483a982702596704d16e2fdba773a13bd8cb6a9
Derek Miranda <derek.miranda.1992@gmail.com>
2017-09-20 19:27:51 -0700

    Added add and remove API methods

--- client/services/moviesApi.js
+++ client/services/moviesApi.js
@@ -10,3 +10,18 @@ export const updateMovies = (movies) => {
   })
     .then(res => res.json())
 }
+
+export const addMovie = (movie) => {
+  return fetch(process.env.MOVIES_API_URL, {
+    method: 'POST',
+    body: movie,
+  })
+    .then(res => res.json())
+}
+
+export const removeMovie = (movie) => {
+  return fetch(`${process.env.MOVIES_API_URL}/${movie.id}`, {
+    method: 'DELETE',
+  })
+    .then(res => res.json())
+}
